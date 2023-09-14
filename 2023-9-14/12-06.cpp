d39346d6ab871ada408f8b6e6b455712123a725b
HRLBZE <1214899196@qq.com>
2023-09-14 12:08:21 +0800

    添加项目文件。

---
+++ 2023-9-14/12-06.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+	using namespace std;
+	//²âÊÔ×Ö³¤
+	cout << sizeof(int) << endl;
+	int youispig;
+	cout << sizeof youispig << endl;
+	cout << sizeof(long int) << endl;
+	cout << sizeof(char) << endl;
+	return 0;
+}
